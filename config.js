5771df4c9eddea5e20585c9375d475873c582f63
Adnan Kasem <adnankasem2017@gmail.com>
2021-09-15 21:43:14 -0700

    OAuth with Passport and Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '250593223621906',
+        clientSecret: 'd3b3d834264820b62f6e10d8fe20c175'
+    }
 }
